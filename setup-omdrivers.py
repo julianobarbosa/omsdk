7db97418d5204a6a52ae49e3e3e90373e20dfb88
rajeevarakkal <36444805+rajeevarakkal@users.noreply.github.com>
2018-02-20 22:52:07 -0800

    license details corrected

--- setup-omdrivers.py
+++ setup-omdrivers.py
@@ -36,7 +36,7 @@ setup(
     author_email='vaideeswaran_ganesan@dell.com',
 
     # Choose your license
-    license='Apache License, Version 2.0',
+    license='Apache Software License',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -51,7 +51,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License, Version 2.0',
+        'License :: OSI Approved :: Apache Software License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
